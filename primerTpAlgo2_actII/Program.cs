04f1ebf6f5be498f681e899f0562fc0adaa9d9c3
Rodrigo91218 <pereira97rodrigo@gmail.com>
2026-04-10 14:33:15 -0300

    .

--- primerTpAlgo2_actII/Program.cs
+++ primerTpAlgo2_actII/Program.cs
@@ -10,7 +10,16 @@ namespace primerTpAlgo2_actII
     {
         static void Main(string[] args)
         {
+            //Se ingresan 10 números. Se pide indicar cuál fue el mayor y cuantas veces se repitió. Utilice funciones.No puede utilizar vectores.
+            
+            double mayor = 0;
+            Console.Write("Ingrese 10 números");
+            mayor = double.Parse(Console.ReadLine());
 
+            for(int i = 0; i <= 10; i++)
+            {
+
+            }
         }
     }
 }
